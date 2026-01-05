fded9776712de0bea264ab2c5211130f00e7a141
Nishant Jagatap <cipherious.nps@gmail.com>
2026-01-05 14:32:12 +0000

    modified:   classes.py 	modified:   requirements.txt

--- classes.py
+++ classes.py
@@ -1,6 +1,5 @@
 import random
 
-
 class GameRNG:
     def __init__(self, seed):
         self.rng = random.Random(seed)
@@ -8,3 +7,14 @@ class GameRNG:
     def chance(self, p):
         return self.rng.random() < p
 
+class sector:
+    def __init__(self, population, rivers, power, absorption, infra, slope, health):
+        self.population = population
+        self.rivers = rivers
+        self.power = power
+        self.absorption = absorption
+        self.infra = infra
+        self.slope = slope
+        self.flooded = 0
+        self.health = health
+        self.deaths = 0
