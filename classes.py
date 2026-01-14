de7aa043adb374bc9ab812b6cca1613a636c4b2c
Nishant Jagatap <cipherious.nps@gmail.com>
2026-01-14 01:09:10 +0000

    modified:   classes.py 	modified:   main.py

--- classes.py
+++ classes.py
@@ -11,6 +11,7 @@ class GameRNG:
     def chance(self, p):
         return self.rng.random() < p
 
+rng = GameRNG(8)
 class sector:
     def __init__(self, name,  population, power, absorption, infra, slope, health, coords):
         self.name = name
@@ -23,5 +24,35 @@ class sector:
         self.health = health
         self.deaths = 0
         self.coords = coords
+
+class river:
+    def __init__(self, name, path):
+        self.name = name
+        self.path = {}
+        self.path.fromkeys(path, 0.5)
+    
+    def flood(self):
+        pass
+
+
+class dam:
+    def __init__(self, name, capacity, cap_used, cost, fail_prob, river, coords, state):
+        self.name = name
+        self.capacity = capacity
+        self.cap_used = cap_used
+        self.cost = cost
+        self.fail_prob = fail_prob
+        self.river = river
+        self.coords = coords
+        self.state = state
+
+    def fail(self):
+        prob = self.fail_prob * self.cap_used / self.capacity
+        chance = rng.chance(prob)
+        if chance:
+            self.river.flood(self.capacity, self.coords)
+            self.state = "Failed"
+
+
 
 game_map = {sector("Guwahati", 3000000, 100, 30, 100, 0, 100): (0,0)}
--- main.py
+++ main.py
@@ -32,6 +32,13 @@ for sector in classes.game_map:
 
 graphics.gui_tick()
 
+boats = {"Guwahati" : 1000}
+dams = {classes.dam("Ranganadi", 10000, 3000, 35, 0.15, "Subansiri", "Upper Subansiri", "Built")}
+potential_dams = {"A": {"capacity" : 3000, "failure" : 0.1, "cost": 30}}
+political = 100
+money = 100
+helicopter = 2
+
 def quitting():
     confirm = input("Input QWERTY to quit. Progress won't be saved ")
     if confirm == "QWERTY":
@@ -46,13 +53,54 @@ def show_dams():
     pass
 
 def deploy_boats():
+    print("You are reassigning x boats from sector A to sector B")
+    x = int(input("Input x "))
+    A = int(input("Input A "))
+
+    if boats[A] < x:
+        print("Insufficient boats to reassign in A")
+    else: 
+        B = int(input("Input B "))
+        boats[A] -= x
+        boats[B] += x
+        print("Operation successful")
+
+def build_dam():
+    dam_build = input("Name of dam to build")
+    if dam_build not in potential_dams:
+        print("Dam not found. Check your spelling!")
+        return
+
+    if money < potential_dams[dam_build]["cost"]:
+        print("Insufficient resources")
+    else:
+        money -=  potential_dams[dam_build]["cost"]
+        dams[dam_build] = potential_dams[dam_build]
+        del potential_dams[dam_build]
+
+def deploy_food():
     pass
 
 commands = {"quit": quitting, "show-boats": show_boats, 
-            "show-dams": show_dams,"deploy-boats": deploy_boats, "": show_dams, "show-dams": show_dams}
+            "show-dams": show_dams,"deploy-boats": deploy_boats, "build-dam": build_dam, "deploy-food": deploy_food}
 
-while True:
-    command = input("Issue your command ")
-    commands[command.lower()]()
+def run_turn():
+    while True:
+        command = input("Issue your command ")
+        if command == "end-turn":
+            break
+        if command not in commands:
+            print("Invalid command")
+        else:
+            commands[command.lower()]()
 
-    
+
+def post_turn():
+    for river in classes.rivers:
+        river.flood()
+
+    for dam in dams:
+        dam.fail()
+
+    for sector in classes.gamemap:
+        sector.health =- sector.flooded*0.1
